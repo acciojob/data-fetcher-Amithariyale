548c56467f37e3ca1c527cff45613ded8ad75f8c
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-02-22 14:20:24 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState("");
+  const fetchData = async () => {
+    try {
+      const response = await axios("https://dummyjson.com/products");
+      console.log(response.data);
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return loading ? <b>Loading...</b> : <div>{JSON.stringify(data)}</div>;
 }
 
 export default App
